10ad77f450508aed1837c86992af4ffe9b532185
Patrick Spencer <pespencer98@gmail.com>
2018-08-08 03:41:09 +0000

    Adding final project B

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,49 +1,49 @@
-
-
-import junit.framework.TestCase;
-
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
 
-
-
-
-
-public class UrlValidatorTest extends TestCase {
+public class UrlValidatorTest {
 
 
    public UrlValidatorTest(String testName) {
-      super(testName);
+      //super(testName);
    }
-
    
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+      //You can use this function to implement your manual testing	   
+	   System.out.println("manual test");
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
-
+      //You can use this function to implement your First Partition testing
+      System.out.println("p1 test");
    }
+   
    
    public void testYourSecondPartition(){
 		 //You can use this function to implement your Second Partition testing	   
-
+      System.out.println("p2 test");
    }
    //You need to create more test cases for your Partitions if you need to 
+   
    
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
-
+      System.out.println("programming test");
    }
    
 
-
+   public static void main(String[] argv) {
+	   UrlValidatorTest fct = new UrlValidatorTest("url test");
+	   
+	   fct.testManualTest();
+	   fct.testYourFirstPartition();
+	   fct.testYourSecondPartition();
+	   fct.testIsValid();
+   }
 }
