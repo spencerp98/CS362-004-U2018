9104a9bc1543c1f738ad9d5b1a1ac4be45bc48e1
Patrick Spencer <pespencer98@gmail.com>
2018-08-10 21:37:38 +0000

    Added manual tests to URLValidatorIncorrect/test/URLValidatorTest.java

--- FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
@@ -32,7 +32,7 @@ public class UrlValidatorTest extends TestCase {
    }
 
    @Override
-protected void setUp() {
+   protected void setUp() {
       for (int index = 0; index < testPartsIndex.length - 1; index++) {
          testPartsIndex[index] = 0;
       }
--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -3,47 +3,71 @@
 // Again, it is up to you to use this file or not!
 
 public class UrlValidatorTest {
-
-
    public UrlValidatorTest(String testName) {
-      //super(testName);
+      
    }
    
-   
-   public void testManualTest()
-   {
-      //You can use this function to implement your manual testing	   
-	   System.out.println("manual test");
+   public void testEquals(String url, boolean result, boolean expected) {
+      if(value != expected) {
+         System.out.println("Failed- " + url);
+         System.out.println("    expected: " + expected);
+         System.out.println("    result: " + result);
+         System.out.println();
+      }
+   }
+
+   public void testManualTest(resultPairs[] tests) {	   
+	   System.out.println("Starting Manual Test");
+	   
+	   // Set up validator object
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   // loop through manual test resultPairs and see if they pass
+	   boolean result;
+	   boolean expected
+	   for(int i = 0; i < tests.length; i++) {
+	      result = urlVal.isValid(tests[i].item);
+	      expected = tests[i].valid;
+	      testEquals(tests[i].item, result, expected);
+	   }
+	   
+	   System.out.println("End of Manual Test");
    }
    
-   
-   public void testYourFirstPartition()
-   {
+   public void testYourFirstPartition() {
       //You can use this function to implement your First Partition testing
-      System.out.println("p1 test");
+      System.out.println("Starting First Partition Test");
+      
    }
    
-   
-   public void testYourSecondPartition(){
+   public void testYourSecondPartition() {
 		 //You can use this function to implement your Second Partition testing	   
-      System.out.println("p2 test");
+      System.out.println("Starting Second Partition Test");
+      
    }
    //You need to create more test cases for your Partitions if you need to 
    
-   
-   public void testIsValid()
-   {
+   public void testIsValid() {
 	   //You can use this function for programming based testing
-      System.out.println("programming test");
+      System.out.println("Starting Programming Based Test");
+      
    }
-   
 
    public static void main(String[] argv) {
 	   UrlValidatorTest fct = new UrlValidatorTest("url test");
 	   
-	   fct.testManualTest();
+	   fct.testManualTest(manualTestPairs);
 	   fct.testYourFirstPartition();
 	   fct.testYourSecondPartition();
 	   fct.testIsValid();
    }
+   
+   ResultPair[] manualTestPairs = {  new ResultPair("http://www.google.com", true),
+                                        new ResultPair("http://www.google.com/", true),
+                                        new ResultPair("https://www.oregonstate.edu/", true),
+                                        new ResultPair("3ht://www.google.com:80/test1?action=view", false),
+                                        new ResultPair("http://google:-80/test1?action=view", false),
+                                        new ResultPair("http://www.google.com:80/..?action=view", false)};
+                                        
+   
 }
